2d72e0ebb3818673ca3d7285b2d12bde7b921b51
vikramshinde1210 <shindevikram1210@gmail.com>
2024-05-19 14:01:57 +0530

    Implement cascade delete functionality for videos

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+import { Comment } from "./comment.model";
+import { Playlist } from "./playlist.model";
+import { Like } from "./like.model";
 
 const videoSchema = new mongoose.Schema({
     videoFile : {
@@ -37,5 +40,34 @@ const videoSchema = new mongoose.Schema({
 }, {timestamps: true})
 
 videoSchema.plugin(mongooseAggregatePaginate) // add mongodb aggregation library as plugin in video schema
+
+// middleware to delete all the data related to the video
+videoSchema.post('findOneAndDelete', { document: true, query: false }, async function(doc, next) {
+    const videoId = doc._id;
+
+    // Remove the video from playlists
+    await Playlist.updateMany(
+        { videos: videoId },
+        { $pull: { videos: videoId } }
+    );
+
+    // Delete all comments associated with the video
+    await Comment.deleteMany({videos: videoId})
+
+    // Remove the video reference from likes
+    await Like.findByIdAndUpdate(
+        {video: videoId},
+        {
+            $unset: {
+                video: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    next();
+});
 
 export const Video = mongoose.model("Video", videoSchema)
